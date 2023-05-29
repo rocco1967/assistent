7ca9bdfd11634a11d85765d77ce42f9ddaaa9b2c
rocco1967 <gianfranco.fa@gmail.com>
2023-05-29 13:50:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if uploaded_file is not None:
         f.write(csv_data)
 
     df = pd.read_csv(uploaded_file.name)
-    st.dataframe(df.head(5))
+    st.dataframe(df.head(10))
 
     chat = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
     agent = create_pandas_dataframe_agent(chat, df, verbose=True)
